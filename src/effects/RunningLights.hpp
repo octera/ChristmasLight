55f4ef654499507b882ace3937531140dae3a690
ROGER Mathieu <mroctera@gmail.com>
2019-11-30 14:14:55 +0100

    End of RunningLight

--- src/effects/RunningLights.hpp
+++ src/effects/RunningLights.hpp
@@ -7,14 +7,16 @@ class RunningLights final : public BaseEffect
 {
 public:
   CRGB color;
-  float rate = 0.1;       // rate of change of hue on each loop
+  float rate = 0.2;       // rate of change of hue on each loop
+  float waveNumber;
 
-  RunningLights(const char* name, CRGB color = CRGB::White ) : BaseEffect(name, 2 * JSON_NODE_SIZE), color(color) { };
+  RunningLights(const char* name, CRGB color = CRGB::White, float waveNumber = 4 ) : 
+    BaseEffect(name, 2 * JSON_NODE_SIZE), color(color), waveNumber(waveNumber) { };
 
   void deserialize(JsonObject& data) override {
     // deltaHue
     if (data.containsKey("color_rgb")) {
-      LEDEFFECT_DEBUG_PRINT(F("SolidEffect: color_rgb to ["));
+      LEDEFFECT_DEBUG_PRINT(F("RunningLights: color_rgb to ["));
       LEDEFFECT_DEBUG_PRINT(data["color_rgb"][0].as<uint8_t>());
       LEDEFFECT_DEBUG_PRINT(F(", "));
       LEDEFFECT_DEBUG_PRINT(data["color_rgb"][1].as<uint8_t>());
@@ -24,7 +26,7 @@ public:
       color = CRGB(data["color_rgb"][0], data["color_rgb"][1], data["color_rgb"][2]);
     // color hsv
     } else if (data.containsKey("color_hsv")) {
-      LEDEFFECT_DEBUG_PRINT(F("SolidEffect: color_hsv to ["));
+      LEDEFFECT_DEBUG_PRINT(F("RunningLights: color_hsv to ["));
       LEDEFFECT_DEBUG_PRINT(data["color_hsv"][0].as<uint8_t>());
       LEDEFFECT_DEBUG_PRINT(F(", "));
       LEDEFFECT_DEBUG_PRINT(data["color_hsv"][1].as<uint8_t>());
@@ -37,8 +39,15 @@ public:
     // rate
     if (data.containsKey("rate")) {
       LEDEFFECT_DEBUG_PRINT(F("RunningLights: rate to "));
-      LEDEFFECT_DEBUG_PRINTLN(data["rate"].as<int8_t>());
-      rate = data["rate"].as<int8_t>();
+      LEDEFFECT_DEBUG_PRINTLN(data["rate"].as<float>());
+      rate = data["rate"].as<float>();
+    }
+
+        // rate
+    if (data.containsKey("waveNumber")) {
+      LEDEFFECT_DEBUG_PRINT(F("RunningLights: waveNumber to "));
+      LEDEFFECT_DEBUG_PRINTLN(data["waveNumber"].as<float>());
+      waveNumber = data["waveNumber"].as<float>();
     }
   }
 
@@ -48,12 +57,14 @@ public:
     color_rgb.add(color.g);
     color_rgb.add(color.b);
     data["rate"] = rate;
+    data["waveNumber"] = waveNumber;
   }
 
   void loop() override {
     _position += rate;
+    float sinRatio = 2 * PI / _controller->size() * waveNumber;
     for(int i=0; i<_controller->size(); i++) {
-        float level = sin(i+_position) * 127 + 128;
+        float level = sin((i+_position)*sinRatio) * 127 + 128;
         _controller->leds()[i] = color%level;
       }
   }
